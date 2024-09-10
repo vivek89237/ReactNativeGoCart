fa77824de20eb02828513862827a2c6ccd6f2a3a
Vivek Kushwaha <vivek89237@gmail.com>
2024-09-10 17:51:59 +0530

    Install and setup

--- app/index.tsx
+++ app/index.tsx
@@ -3,17 +3,19 @@ import { Stack, Link } from 'expo-router';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
-
+import Map from '~/components/Map';
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
+      {/* <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
-      </Container>
+        
+      </Container> */}
+      <Map />
     </>
   );
 }
